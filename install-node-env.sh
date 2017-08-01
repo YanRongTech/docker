0344a006471a6af2eeb4e3c4596428fbccb990e7
yedaodao <404069912@qq.com>
2017-08-01 20:56:43 +0800

    fix shell script bug

--- install-node-env.sh
+++ install-node-env.sh
@@ -2,31 +2,36 @@
 
 download() {
     local nodeVersion
-    nodeVersion="$1"
-
-    if [[ -z nodeVersion ]]; then
-        nodeVersion="v6.11.1"
+    if [[ $# -eq 1 ]] && [[ -n $1 ]]; then
+        nodeVersion="$1"
+    else
+        nodeVersion="v6.11.1";
     fi
+    echo $#;
     doDownload "$nodeVersion"
 }
 
 doDownload() {
-    local url version nodePkgName pkgPath
-    version= "$1"
-    nodePkgName="node-$version-linux-x64"
-    url="https://nodejs.org/dist/$version/$nodePkgName.tar.xz"
+    local url nodeVersion nodePkgName pkgPath
+    nodeVersion="$1"
+    nodePkgName="node-$nodeVersion-linux-x64"
+    url="https://nodejs.org/dist/$nodeVersion/$nodePkgName.tar.xz"
     pkgPath="/tmp"
 
     cd "$pkgPath"
-    echo "Downloading Node.js $version from $url"
+    echo "Downloading Node.js $nodeVersion from $url"
     curl --retry "3" --retry-delay "0" --retry-max-time "5" -s -f -L "$url"
-    return "$nodePkgName"
+    echo "$nodePkgName"
 }
 
 main() {
-    local nodePkgName nodeInstallPath
-    nodeInstallPath = "/usr/lib/"
-    nodePkgName = download $1
+    local nodePkgName nodeInstallPath nodeVersion
+    nodeInstallPath="/usr/lib/"
+    if [[ $# -eq 1 ]]; then
+        nodeVersion="$1";
+    fi
+    nodePkgName=$(download "$nodeVersion")
+    echo "$nodePkgName"
     tar -zxvf "/tmp/$nodePkgName.tar.xz" -C "$nodeInstallPath"
     ln -s "$nodeInstallPath/$nodePkgName/bin/node" "/usr/local/bin/node"
     ln -s "$nodeInstallPath/$nodePkgName/bin/npm" "/usr/local/bin/npm"
