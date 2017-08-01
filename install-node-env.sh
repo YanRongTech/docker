90401b8cc1dbf6ace56fc5787bd79d5d3019034e
yedaodao <404069912@qq.com>
2017-08-01 22:51:41 +0800

    debug

--- install-node-env.sh
+++ install-node-env.sh
@@ -1,40 +1,38 @@
 #!/bin/bash -eu
 
-download() {
-    local nodeVersion
+install() {
+    local nodeVersion nodePkgName nodeInstallPath
     if [[ $# -eq 1 ]] && [[ -n $1 ]]; then
         nodeVersion="$1"
     else
         nodeVersion="v6.11.1";
     fi
-    echo $#;
-    doDownload "$nodeVersion"
+    nodeInstallPath="/usr/lib/"
+    nodePkgName="node-$nodeVersion-linux-x64"
+    doDownload "$nodeVersion $nodePkgName"
+    tar -zxvf "/tmp/$nodePkgName.tar.xz" -C "$nodeInstallPath"
+    ln -s "$nodeInstallPath/$nodePkgName/bin/node" "/usr/local/bin/node"
+    ln -s "$nodeInstallPath/$nodePkgName/bin/npm" "/usr/local/bin/npm"
 }
 
 doDownload() {
     local url nodeVersion nodePkgName pkgPath
     nodeVersion="$1"
-    nodePkgName="node-$nodeVersion-linux-x64"
+    nodePkgName="$2"
     url="https://nodejs.org/dist/$nodeVersion/$nodePkgName.tar.xz"
     pkgPath="/tmp"
 
     cd "$pkgPath"
     echo "Downloading Node.js $nodeVersion from $url"
-    curl --retry "3" --retry-delay "0" --retry-max-time "5" -s -f -L "$url"
-    echo "$nodePkgName"
+    curl --retry "3" --retry-delay "0" --retry-max-time "5" -# -O "$url"
 }
 
 main() {
-    local nodePkgName nodeInstallPath nodeVersion
-    nodeInstallPath="/usr/lib/"
+    local nodeVersion
     if [[ $# -eq 1 ]]; then
         nodeVersion="$1";
     fi
-    nodePkgName=$(download "$nodeVersion")
-    echo "$nodePkgName"
-    tar -zxvf "/tmp/$nodePkgName.tar.xz" -C "$nodeInstallPath"
-    ln -s "$nodeInstallPath/$nodePkgName/bin/node" "/usr/local/bin/node"
-    ln -s "$nodeInstallPath/$nodePkgName/bin/npm" "/usr/local/bin/npm"
+    install "$nodeVersion"
 }
 
 main "$@"
